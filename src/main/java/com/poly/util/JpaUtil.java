16ae26dbe93372bcb9f2a77b447f0a2ce48b76ea
PhuongNTK <phuongntk2392@gmail.com>
2023-07-05 22:00:38 +0700

    create JpaUtils

---
+++ src/main/java/com/poly/util/JpaUtil.java
@@ -0,0 +1,21 @@
+package com.poly.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+	private static EntityManagerFactory factory;
+	static public EntityManager getEntityManager() {
+		if(factory == null || !factory.isOpen()) {
+			factory = Persistence.createEntityManagerFactory("CGVmovie");
+		}
+		return factory.createEntityManager();
+	}
+	static public void shutdown() {
+		if(factory!=null && factory.isOpen()) {
+			factory.close();
+		}
+		factory=null;
+	}
+}
